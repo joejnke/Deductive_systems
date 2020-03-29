64c2aeb2d55fc490c11cacb3856736d2797e7c09
Kirubel <joejnke@gmail.com>
2020-03-29 16:44:53 +0300

    Implement room and its test

--- wumpus_world/types/room.cpp
+++ wumpus_world/types/room.cpp
@@ -11,14 +11,24 @@ room::room(std::pair<int, int> room_id)
 {
     this->room_id = room_id;
 
-    int default_tv = -1;
-    this->knowledge = {{"breeze", default_tv},
-                       {"glitter", default_tv},
-                       {"pit", default_tv},
-                       {"stench", default_tv},
-                       {"visited", default_tv},
-                       {"wumpus", default_tv}};
+    this->default_tv = -1;
+    this->knowledge = {{"breeze", this->default_tv},
+                       {"glitter", this->default_tv},
+                       {"pit", this->default_tv},
+                       {"stench", this->default_tv},
+                       {"visited", this->default_tv},
+                       {"wumpus", this->default_tv}};
 }
+
+std::pair<int, int> room::get_room_id()
+{
+    return this->room_id;
+}
+
+std::map<std::string, int> room::get_knowledge()
+{
+    return this->knowledge;
+}      
 
 bool room::get_truth_value(std::string knowledge_term)
 {
@@ -30,6 +40,21 @@ bool room::get_truth_value(std::string knowledge_term)
 
     else
         throw "truth value not yet set...";
+}
+
+void room::set_truth_value(std::string knowledge_term, bool tv)
+{
+    try
+    {
+        if (tv)
+            this->knowledge.at(knowledge_term) = 1;
+        else
+            this->knowledge.at(knowledge_term) = 0;
+    }
+    catch(const std::out_of_range& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 }
 
 std::set<std::pair<int, int>> room::get_adjacents()
--- wumpus_world/types/room.hpp
+++ wumpus_world/types/room.hpp
@@ -20,9 +20,21 @@ class room
 private:
     std::map<std::string, int> knowledge; // truth values for knowledge contained in a room
     std::pair<int, int> room_id;          // room_id of the room. Assumes left-bottom corner as (x,y)=(1,1) and starting point.
+    int default_tv;                       // default truth value
 public:
-    /// Constructor initializing the room_id to input parameter and all truth values to false
+    /// Constructor initializing the default_tv to -1,
+    /// room_id to input parameter and all truth values to false
     room(std::pair<int, int> room_id);
+    /**
+        *  @brief Getter of the room_id class attribute.
+        *  @return pair<int, int>
+        */
+    std::pair<int, int> get_room_id();
+    /**
+        *  @brief Getter of the knowledge class attribute.
+        *  @return map<std::string, int> 
+        */
+    std::map<std::string, int> get_knowledge();
     /**
         *  @brief Getter of the ruth value associated with the given knowledge_term.
         *  @param knowledge_term String of the key of whose truth value is required. 
@@ -34,8 +46,18 @@ public:
         */
     bool get_truth_value(std::string knowledge_term);
     /**
+        *  @brief Getter of the ruth value associated with the given knowledge_term.
+        *  @param knowledge_term String of the key of whose truth value is required. 
+        *  @param tv Bool to set as truth value.
+        * 
+        *  Maps the integers -1, 0, 1 to NaN, false, true. It returns if the truth
+        *  value is false or true. Incase of -1, it throws exception of "value not
+        *  yet set"
+        */
+    void set_truth_value(std::string knowledge_term, bool tv);
+    /**
         *  @brief Gets set of room_ids of valid neighbouring rooms.
-        *  @return Set 
+        *  @return Set get_adjacents
         * 
         *  Find the room_id of all valid neighbouring rooms to the room and return
         *  a set of these room_ids.
