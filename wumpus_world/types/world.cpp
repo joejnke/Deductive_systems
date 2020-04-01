db3b7a2a071c7cfaab7ed31bdb97986e71edd0d8
Tadele01 <tadeleyednkachw@gmail.com>
2020-04-01 18:04:41 -0400

    bugs fixed in world class

--- wumpus_world/types/world.cpp
+++ wumpus_world/types/world.cpp
@@ -1,9 +1,8 @@
 #include "world.hpp"
 
-
 using namespace std;
 
-World::World()
+world::world()
 {
 	grid_init();
 	pair<int, int> wumpus_room = this->wumpus_room;
@@ -13,116 +12,128 @@ World::World()
 	v.push_back(1);
 	v.push_back(1);
 	v.push_back(0);
-	this->agent_status[agent_room] = v;  
-	
+	this->agent_status[agent_room] = v;
 }
 
-void World::grid_init(){
+void world::grid_init()
+{
 	srand((int)time(0));
-    int random_number ;
+	int random_number;
+	int size = 4;
+	int x, y;
 	vector<pair<int, int>> store;
-    pair<int, int> agent_room_id;      // to hold the agent room_id
-    pair<int, int> restricted_position; //to hold the restricted position adjcent to the agent
-	pair<int, int> restrictedPosition2; //to hold the restricted position adjcent to the agent
-    agent_room_id = make_pair(1, 1);
-    restricted_position = make_pair(1, 2);
-    restrictedPosition2 = make_pair(2, 1);
+	pair<int, int> agent_room_id;		// to hold the agent room_id
+	pair<int, int> restricted_position; //to hold the restricted position adjcent to the agent
+	pair<int, int> resticted_position2; //to hold the restricted position adjcent to the agent
+	agent_room_id = make_pair(1, 1);
+	restricted_position = make_pair(1, 2);
+	resticted_position2 = make_pair(2, 1);
 
 	room agent_initial_room(agent_room_id);
-	room restricted_room (restricted_position);
-	room restricted_room2 (restricted_position2);
+	room restricted_room(restricted_position);
+	room restricted_room2(resticted_position2);
 
-	this->grid.push_back(agent_initial_room);
-	this->grid.push_back(restricted_room);
-	this->grid.push_back(restricted_room2);
+	this->grid[1][1] = agent_initial_room;
+	this->grid[1][2] = restricted_room;
+	this->grid[2][1] = restricted_room2;
 
-    while (store.size() < 8)
-    {
-        int x, y;
+	while (store.size() < 13)
+	{
+		random_number = rand() % size;
+		x = random_number + 1;
+		random_number = rand() % size;
+		y = random_number + 1;
 
-        random_number = rand() % size;
-        x = random_number + 1;
-        random_number = rand() % size;
-        y = random_number + 1;
-        pair<int, int> newpair = make_pair(x, y);
-        if (newpair != restricted_position && newpair != resticted_position2 && newpair != agent_room_id)
-        {
-            store.push_back(newpair);
-            sort(store.begin(), store.end());
-            store.erase(unique(store.begin(), store.end()), store.end());
-        }
-    }
-    
+		pair<int, int> newpair = make_pair(x, y);
+		if (newpair != restricted_position && newpair != resticted_position2 && newpair != agent_room_id)
+		{
+			store.push_back(newpair);
+			sort(store.begin(), store.end());
+			store.erase(unique(store.begin(), store.end()), store.end());
+		}
+	}
 
-    vector<pair<int, int>>::iterator it = store.end();
-    --it;
-    pair<int, int> pit3 = *it; //random position for pit
-    --it;
-    pair<int, int> wumpus = *it; //random position for wumpus
-    --it;
-    pair<int, int> pit2 = *it; //random position for pit
-    --it;
+	vector<pair<int, int>>::iterator it = store.end();
+	--it;
+	pair<int, int> pit3 = *it; //random position for pit
+	--it;
+	pair<int, int> wumpus = *it; //random position for wumpus
+	--it;
+	pair<int, int> pit2 = *it; //random position for pit
+	--it;
 
-    pair<int, int> gold = *it; //random position for gold
-    --it;
-    pair<int, int> pit1 = *it; //random position for pit
+	pair<int, int> gold = *it; //random position for gold
+	--it;
+	pair<int, int> pit1 = *it; //random position for pit
 
-	pair< int, int> new_room_pair;
-	for(pair<int, int> i : store){
+	pair<int, int> new_room_pair;
+	for (pair<int, int> i : store)
+	{
 		new_room_pair = make_pair(i.first, i.second);
-		if (new_room_pair == pit1 || new_room_pair == pit2 || new_room_pair == pit3 || new_room_pair == wumpus || new_room_pair == gold){
+		if (new_room_pair == pit1 || new_room_pair == pit2 || new_room_pair == pit3 || new_room_pair == wumpus || new_room_pair == gold)
+		{
 			continue;
 		}
 		room new_room(new_room_pair);
-		this->grid.push_back(new_room);
+		this->grid[new_room_pair.first][new_room_pair.second] = new_room;
 	}
-    put_wumpus(wumpus);
 
-    put_pit(pit1);
+	put_wumpus(wumpus);
 
-    put_pit(pit2);
+	put_pit(pit1);
 
-    put_pit(pit3);
+	put_pit(pit2);
 
-    put_gold(gold);
+	put_pit(pit3);
+
+	put_gold(gold);
 }
-
-map<pair<int, int>, vector<int> > World::get_agent_status(){
+auto world::get_grid()
+{
+	return this->grid;
+}
+map<pair<int, int>, vector<int>> world::get_agent_status()
+{
 	return this->agent_status;
 }
-map<pair<int, int>, int> World::get_wumpus_status(){
+map<pair<int, int>, int> world::get_wumpus_status()
+{
 	return this->wumpus_status;
 }
-pair<int, int> World::get_wumpus_room(){
+pair<int, int> world::get_wumpus_room()
+{
 	return this->wumpus_room;
 }
-pair<int, int> World::get_agent_room(){
+pair<int, int> world::get_agent_room()
+{
 	return this->agent_room;
 }
-pair<int, int> World::get_gold_room(){
+pair<int, int> world::get_gold_room()
+{
 	return this->gold_room;
 }
-pair<int, int> World::get_pit_room(){
-	return this->pit_room;
-}
 
-void put_pit(pair<int, int> pit){
+void world::put_pit(pair<int, int> pit)
+{
 	room pit_room(pit);
 	pit_room.set_truth_value("pit", 1);
-	this->grid.push_back(pit_room);
+	this->grid[pit.first][pit.second] = pit_room;
 }
-void put_gold(pair<int, int> gold){
+void world::put_gold(pair<int, int> gold)
+{
+	this->gold_room = gold;
 	room gold_room(gold);
 	gold_room.set_truth_value("glitter", 1);
-	this->grid.push_back(gold_room);
+	this->grid[gold.first][gold.second] = gold_room;
 }
-void put_wumpus(pair<int, int> wumpus){
+void world::put_wumpus(pair<int, int> wumpus)
+{
+	this->wumpus_room = wumpus;
 	room wumpus_room(wumpus);
 	wumpus_room.set_truth_value("wumpus", 1);
-	this->wumpus_room = wumpus;
-	this->grid.push_back(wumpus_room);
+	this->grid[wumpus.first][wumpus.second] = wumpus_room;
 }
-
-void kill_wumpus_at(pair<int, int> room_id){
+void world::kill_wumpus_at(pair<int, int> room_id)
+{
 	this->wumpus_status[room_id] = 0;
 }
--- wumpus_world/types/world.hpp
+++ wumpus_world/types/world.hpp
@@ -1,6 +1,6 @@
 
-#ifndef WORLD_H_
-#define WORLD_H_
+#ifndef world_H_
+#define world_H_
 
 #include <string>
 #include <vector>
@@ -8,47 +8,45 @@
 #include <bits/stdc++.h>
 #include <set>
 #include <ctime>
-#include<cstdlib>
+#include <cstdlib>
 #include "room.hpp"
 #include <utility>
 
 using namespace std;
 
-class World
+class world
 {
 private:
-	int model;// until modle implementation done considerd it as integer
-	pair<int, int> agent_room ;
+	int model; // until modle implementation done considerd it as integer
+	pair<int, int> agent_room;
 	pair<int, int> wumpus_room;
 	pair<int, int> gold_room;
-	pair<int, int> pit_room;
-	vector<room> grid; //2d array of rooms
+	room grid[5][5]; //2d array of rooms segementation fault happens if we use 4*4
 public:
 	//map to store agent status  current_room_id(pair of points ) ,living(0 or 1 ),  remaining_arrow(0 or 1), found_gold(0 or 1 as boolean)
-	map<pair<int, int>, vector<int> > agent_status; 
-	//map to store wumpus status living(0 or 1 ), wumpus_room_id (pair of points ) 
-	map<pair<int, int >, int> wumpus_status;
-	//consutructor for World class 
-    World();
-   //Initialize the grid with random knowledge set to the rooms.
- 	void grid_init();
+	map<pair<int, int>, vector<int>> agent_status;
+	//map to store wumpus status living(0 or 1 ), wumpus_room_id (pair of points )
+	map<pair<int, int>, int> wumpus_status;
+	//consutructor for world class
+	world();
+	//Initialize the grid with random knowledge set to the rooms.
+	void grid_init();
 	//since for now we consider model as int the method return int datatype
 	int get_model();
 	//return a copy of grid attribute
-	vector get_grid();
+	auto get_grid();
 	//return copy of the room object at the given room_id from the grid
-	Room get_room<pair<int, int> room_id);
+	room get_room(pair<int, int> room_id);
 	//return copy of agent_status
 	map<pair<int, int>, vector<int>> get_agent_status();
 	//or we can do  auto get_agent_status();
-	
+
 	//return copy of wumpus status
-	map<pair<int, int> , int> get_wumpus_status();
+	map<pair<int, int>, int> get_wumpus_status();
 	//kill wumpus if it found in the specified room id
 	pair<int, int> get_wumpus_room();
 	pair<int, int> get_agent_room();
 	pair<int, int> get_gold_room();
-	pair<int, int> get_pit_room();
 	void put_pit(pair<int, int>);
 	void put_gold(pair<int, int>);
 	void put_wumpus(pair<int, int>);
